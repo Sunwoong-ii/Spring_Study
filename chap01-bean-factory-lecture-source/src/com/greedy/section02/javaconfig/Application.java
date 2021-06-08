02aa1e11c00d89119c62aed41cc844705e756d69
kim-sunwoong <sw649666@gmail.com>
2021-06-08 17:19:59 +0900

    bean-factory study

---
+++ chap01-bean-factory-lecture-source/src/com/greedy/section02/javaconfig/Application.java
@@ -0,0 +1,19 @@
+package com.greedy.section02.javaconfig;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new AnnotationConfigApplicationContext(ContextConfiguration.class);
+		
+		System.out.println(context);
+		
+		MemberDTO member = (MemberDTO)context.getBean("member");
+		System.out.println(member);
+		
+	}
+	
+}
