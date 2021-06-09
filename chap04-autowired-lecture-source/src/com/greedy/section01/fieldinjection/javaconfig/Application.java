77e3cc1ad2d5b89c4a4e663d7af6013e6439129b
kim-sunwoong <sw649666@gmail.com>
2021-06-09 11:43:55 +0900

    autowired study 21-06-09

---
+++ chap04-autowired-lecture-source/src/com/greedy/section01/fieldinjection/javaconfig/Application.java
@@ -0,0 +1,15 @@
+package com.greedy.section01.fieldinjection.javaconfig;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * @AutoWired를 이용한 의존성 주입
+		 * 
+		 * 스프링 3.1이후 버전부터 어노테이션을 이용한 bean 생성 및 의존성 주입을 위한 어노테이션을 제공한다.
+		 * 어노테이션 방식을 이용하면 지금까지 본 생성자 주입과 setter주입 외에필드 주입을 추가적으로 할 수 있다.
+		 */
+	}
+
+}
